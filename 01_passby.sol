e791a8e3676cdd0ef8926e64b93016a212765596
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2019-04-13 16:51:47 +0700

    Initial commit

---
+++ 01_passby.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.5.0;
+
+
+// Adapt from Data location and assignment behaviour, Chapter 3, Solidity Documentation, Release 0.5.8
+contract PassBy {
+    
+    uint[] public publicStorageArray;
+    
+    event Logging(string key, uint[] val);
+    event StateChanges(string msg);
+    
+    modifier notEmptyArray(uint[] memory memoryArray) {
+        require(memoryArray.length > 0, "array cannot be empty");
+        _;
+    }
+    
+    modifier firstElementNot1Or2(uint[] memory memoryArray) {
+        require(memoryArray[0] != 1, "array[0] must not be 1");
+        require(memoryArray[0] != 2, "array[0] must not be 2");
+        _;
+    }
+    
+    
+    // Shows that memory -> storage will pass by value, but same type will pass by referemce.
+    function f(uint[] memory memoryArray) public notEmptyArray(memoryArray) {
+        emit Logging("memoryArray", memoryArray);
+        publicStorageArray = memoryArray;
+        emit Logging("publicStorageArray", publicStorageArray);
+        uint[] memory anotherMemoryArray = memoryArray;
+        emit Logging("anotherMemoryArray", anotherMemoryArray);
+        uint[] storage anotherStorageArray = publicStorageArray;
+        emit Logging("anotherStorageArray", anotherStorageArray);
+        
+        memoryArray[0] = 1;
+        emit StateChanges("memoryArray[0] changes to 1");
+        emit Logging("memoryArray", memoryArray);
+        emit Logging("publicStorageArray", publicStorageArray);
+        emit Logging("anotherMemoryArray", anotherMemoryArray);
+        emit Logging("anotherStorageArray", anotherStorageArray);
+        
+        publicStorageArray[0] = 2;
+        emit StateChanges("publicStorageArray[0] changes to 2");
+        emit Logging("memoryArray", memoryArray);
+        emit Logging("publicStorageArray", publicStorageArray);
+        emit Logging("anotherMemoryArray", anotherMemoryArray);
+        emit Logging("anotherStorageArray", anotherStorageArray);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# My First Solidity
+
+This repository contains my very first own Solidity code.
